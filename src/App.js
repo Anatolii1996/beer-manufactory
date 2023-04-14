7629d556dbfe82d4bd3e4cf2d9e62d62f7c018d9
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-04-14 19:47:51 +0300

    добавил хедер

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import './App.scss';
+// import { Route, Routes } from 'react-router-dom';
+import Header from "./component/header";
+import Home from "./component/home";
 
 function App() {
   return (
     <div className="App">
-      <div className='wrapper'></div>
+      <div className="wrapper">
+        <Header/>
+      </div>
     </div>
   );
 }
---
+++ src/component/home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div className="App">
+      <div className="wrapper">
+       <p>Home</p>
+      </div>
+    </div>
+  );
+};
+export default Home;
