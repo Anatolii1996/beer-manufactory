5eb8b8feb50ea54cea34fa9a163392f966e51c04
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-04-15 16:04:02 +0300

    поправил иконку

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,13 @@
+import { CiWheat } from "react-icons/ci";
+
 const Home = () => {
   return (
-   <div className="container-fluid home">
-    <div className="home_wrap"></div>
-   </div>
+    <div className="container-fluid home">
+      <div className="home_wrap">
+        <CiWheat />
+      </div>
+      
+    </div>
   );
 };
 export default Home;
