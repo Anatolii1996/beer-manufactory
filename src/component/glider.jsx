5a6e7baaa432c96c1b141e382070af3efa27a4d3
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-04-15 19:30:24 +0300

    добавил автоматический возврат слайдера

--- src/component/glider.jsx
+++ src/component/glider.jsx
@@ -4,43 +4,49 @@ import { cards } from "./cards";
 import Card from "./card";
 
 const GliderWrap = () => {
-    useEffect(() => {
-        const glider = new Glider(document.querySelector(".glider"), {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          scrollLockDelay: 1000, // Задержка в миллисекундах
-          duration: 3,
-          draggable: true,
-          dots: ".dots",
-          arrows: {
-            prev: ".glider-prev",
-            next: ".glider-next",
-          },
-          // Дополнительные опции
-          // ...
-        });
-    
-        // Функция автоматической прокрутки слайдера
-        const autoScroll = () => {
-          glider.scrollItem(glider.slide + 1);
-        };
-    
-        // Запустить автоматическую прокрутку с интервалом в 3000 миллисекунд (3 секунды)
-        const interval = setInterval(autoScroll, 3000);
-    
-        // Остановить автоматическую прокрутку при размонтировании компонента
-        return () => {
-          clearInterval(interval);
-        };
-      }, []);
+  useEffect(() => {
+    const glider = new Glider(document.querySelector(".glider"), {
+      slidesToShow: 3,
+      slidesToScroll: 1,
+
+      duration: 3,
+      draggable: true,
+      dots: ".dots",
+      arrows: {
+        prev: ".glider-prev",
+        next: ".glider-next",
+      },
+      rewind: true,
+    });
+
+    // Функция автоматической прокрутки слайдера
+    const autoScroll = () => {
+      glider.scrollItem(glider.slide + 1);
+      
+      if (glider.slide  === cards.length - 2) {
+        console.log(12365);
+        glider.scrollItem(0, true);
+      }
+    };
+
+    // Запустить автоматическую прокрутку с интервалом в 3000 миллисекунд (3 секунды)
+    const interval = setInterval(autoScroll, 3000);
+
+    // Остановить автоматическую прокрутку при размонтировании компонента
+    return () => {
+      clearInterval(interval);
+    };
+  });
 
   return (
     <div className="glider-contain">
       <div className="glider">
         {cards.map((el, index) => {
-          return <div key={index}>
-            <Card  title={el.title} desription={el.description} img={el.img}/>
-          </div>;
+          return (
+            <div key={index}>
+              <Card title={el.title} desription={el.description} img={el.img} />
+            </div>
+          );
         })}
       </div>
 
