28147d83289735a1aceb74a2966a4f3acc7143e5
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-04-15 16:17:15 +0300

    добавил иконки

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,16 @@
 import { CiWheat } from "react-icons/ci";
+import { BsFire } from "react-icons/bs";
+import { GiBeerStein } from "react-icons/gi";
+
+
 
 const Home = () => {
   return (
     <div className="container-fluid home">
       <div className="home_wrap">
         <CiWheat />
+        <BsFire/>
+        <GiBeerStein/>
       </div>
       
     </div>
